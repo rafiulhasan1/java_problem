62a7cbc6ea4fa115fdb5f5c714fbcbead730734b
rafiulhasan1 <rafiulhasan001@gmail.com>
2023-12-23 13:03:02 +0600

    problem 02 added

---
+++ problem-02.js
@@ -0,0 +1,19 @@
+var pro = 39;
+
+
+
+if(80<=pro){
+    console.log("Rafiul you are A gread");
+}
+else if(79>=pro && 60<=pro){
+    console.log("Rafiul you are B gread");
+}
+else if(59>=pro && 50<=pro){
+    console.log("Rafiul you are C gread");
+}
+else if(49>=pro && 40<=pro){
+    console.log("Rafiul you are D gread");
+}
+else{
+    console.log("Rafiul you are F gread");
+}
