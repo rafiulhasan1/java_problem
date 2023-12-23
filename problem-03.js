484c20c5f24fc427f447194dac30db51a884d6b1
rafiulhasan1 <rafiulhasan001@gmail.com>
2023-12-23 13:08:54 +0600

    problem 03 added

---
+++ problem-03.js
@@ -0,0 +1,13 @@
+var num1 = 13;
+var num2 = 79;
+var num3 = 45;
+
+if(num1>num2 && num1>num3){
+    console.log("largest number : ",num1);
+}
+else if(num2>num1 && num2>num3){
+    console.log("largest number : ",num2);
+}
+else{
+    console.log('largest number : ',num3);
+}
